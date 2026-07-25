7e4dd2aae80b7db0a0f16b2211ca7aa10a69b3b4
MANTHAN SHAH <manthanshahgit@gmail.com>
2026-07-25 16:28:35 +0530

    Refactor MySQL connection to use connection pool

--- studententer.js
+++ studententer.js
@@ -33,12 +33,20 @@ app.set("views", path.join(__dirname, "/views"));
 // });
 
 // hosting
-const connection = mysql.createConnection({
+const mysql = require("mysql2");
+
+const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
+
+module.exports = pool.promise();
 
 // Connect DB
 
