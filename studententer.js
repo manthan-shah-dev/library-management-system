1203b1c0962d112ebf7987ac1e75f7928d281119
MANTHAN SHAH <manthanshahgit@gmail.com>
2026-07-25 14:01:20 +0530

    Refactor MySQL connection to use env variables
    Updated MySQL connection to use environment variables.

--- studententer.js
+++ studententer.js
@@ -25,11 +25,19 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
 // MySQL connection
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'Pass1',
+//     database: 'collegedb'
+// });
+
+// hosting
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Pass1',
-    database: 'collegedb'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 // Connect DB
